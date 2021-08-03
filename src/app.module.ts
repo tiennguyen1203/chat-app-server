25a3ba1ba79e62f0112c6e8085cb8f0520db8941
Tien Nguyen <tien.nguyen@team.enouvo.com>
2021-08-03 22:39:59 +0700

    fix: Restructure api endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MessagesController } from './messages/messages.controller';
-import { MessagesModule } from './messages/messages.module';
-import { UsersModule } from './users/users.module';
-import { AppGateway } from './app.gateway';
-import { RoomsModule } from './rooms/rooms.module';
-import { ChannelsModule } from './channels/channels.module';
+import { ChannelsModule } from 'src/api/channels/channels.module';
+import { MessagesController } from 'src/api/messages/messages.controller';
+import { MessagesModule } from 'src/api/messages/messages.module';
+import { RoomsModule } from 'src/api/rooms/rooms.module';
+import { AppController } from 'src/app.controller';
+import { AppGateway } from 'src/app.gateway';
+import { AppService } from 'src/app.service';
+import { AuthModule } from 'src/auth/auth.module';
 import { Channel, ChannelSchema } from 'src/schemas/channel.schema';
-import { ChannelsService } from './channels/channels.service';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from 'src/api/users/users.module';
 
 @Module({
   imports: [
